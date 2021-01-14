db3051128b689816d3b054d6865fb34f2ae4ef24
ukikagi <ukikagi@gmail.com>
2021-01-14 23:53:16 +0900

    Add simple tests

--- test/Test.hs
+++ test/Test.hs
@@ -1,4 +1,21 @@
 module Main (main) where
 
+import Parser (int)
+import Test.HUnit.Base (Test (TestCase, TestList), assertFailure, (@?=))
+import Test.HUnit.Text (runTestTTAndExit)
+import Text.Parsec (parse)
+
+testParseIntSuccess :: Test
+testParseIntSuccess = TestCase $ parse int "" "12345" @?= Right 12345
+
+testParseIntFail :: Test
+testParseIntFail = TestCase $
+  case parse int "" "abcde" of
+    Left _ -> return ()
+    Right _ -> assertFailure "The parse must fail for non-numeric input."
+
+tests :: Test
+tests = TestList [testParseIntSuccess, testParseIntFail]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = runTestTTAndExit tests
